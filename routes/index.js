89ba58cae82cf7bf096f135fb8f079751f62088c
herlambang0321 <herlambang88888888@gmail.com>
2021-10-12 00:07:31 +0700

    initial pagination

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,28 @@
 var express = require("express");
 var router = express.Router();
-var moment = require('moment');
+var moment = require("moment");
 
 module.exports = function (db) {
   router.get("/", function (req, res, next) {
-    db.query("select * from breaddata", (err, data) => {
+    const page = req.query.page || 1;
+    const limit = 3;
+
+    db.query("select count(*) as total from breaddata", (err, data) => {
       if (err) {
-        return res.send(err)
+        return res.send(err);
       }
-      res.render("home/list", { data: data.rows, moment: moment });
+      const total = data.rows[0].total;
+      const offset = (page - 1) * limit;
+      const pages = Math.ceil(total / limit);
+
+      db.query(
+        `select * from breaddata limit $1 offset $2`, [limit, offset], (err, data) => {
+          if (err) {
+            return res.send(err);
+          }
+          res.render("home/list", { data: data.rows, moment: moment, page, pages });
+        }
+      );
     });
   });
 
