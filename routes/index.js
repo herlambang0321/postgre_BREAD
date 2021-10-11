ee76276337c6ea2449548c57c0ff096c9aa422c9
herlambang0321 <herlambang88888888@gmail.com>
2021-10-11 19:44:21 +0700

    initial title

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+module.exports = function (db) {
 
-module.exports = router;
+  router.get("/", function (req, res, next) {
+    res.render("home/list", { title: "Express" });
+  });
+
+  return router;
+};
