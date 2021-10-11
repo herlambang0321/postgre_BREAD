9baee3f4f7bf4781290330daf93d296c4c66405b
herlambang0321 <herlambang88888888@gmail.com>
2021-10-12 02:44:24 +0700

    initial search

--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,50 @@ var moment = require("moment");
 
 module.exports = function (db) {
   router.get("/", function (req, res, next) {
+    const { stringdata, booleandata } = req.query;
+
+    const url = req.url == '/' ? '/?page=1' : req.url;
     const page = req.query.page || 1;
     const limit = 3;
+    const offset = (page - 1) * limit;
 
-    db.query("select count(*) as total from breaddata", (err, data) => {
+    let params = [];
+
+    if (stringdata) {
+      params.push(`stringdata ilike '%${stringdata}%'`);
+    }
+
+    if (booleandata) {
+      params.push(`booleandata=${booleandata}`);
+    }
+
+    let sql = "select count(*) as total from breaddata";
+
+    if (params.length > 0) {
+      sql += ` where ${params.join(' and ')}`
+    }
+
+    db.query(sql, (err, data) => {
       if (err) {
         return res.send(err);
       }
       const total = data.rows[0].total;
-      const offset = (page - 1) * limit;
       const pages = Math.ceil(total / limit);
 
+      sql = `select * from breaddata`;
+
+      if (params.length > 0) {
+        sql += ` where ${params.join(' and ')}`
+      }
+      
+      sql += ` limit $1 offset $2`
+
       db.query(
-        `select * from breaddata limit $1 offset $2`, [limit, offset], (err, data) => {
+        sql, [limit, offset], (err, data) => {
           if (err) {
             return res.send(err);
           }
-          res.render("home/list", { data: data.rows, moment: moment, page, pages });
+          res.render("home/list", { data: data.rows, moment: moment, page, pages, url, query: req.query });
         }
       );
     });
